6d1afbb3ac3cd8c1f14bb6b67a159eba9c10c0ca
John Brunton <jbrunton@zipcar.com>
2019-08-22 22:44:35 +0100

    some styling

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ function isNumeric(num){
   return !isNaN(num)
 }
 
+var data = {
+  trainingData: [
+    {size: 'S', values: [1, 2, 3, 2]},
+    {size: 'M', values: [3, 3, 5, 8]},
+    {size: 'L', values: [5, 7, 7, 8]}
+  ]
+};
+
 new Vue({
   el: '#app',
   data() {
-      return {
-          tags: [
-              '123',
-              '456',
-              '789'
-          ]
-      }
+      return data;
   },
   methods: {
     beforeAdding(tag) {
